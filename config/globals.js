c5da07ac6267c529371340f149ba1809e74113d3
Mansi <mansipatel_23@yahoo.in>
2018-08-11 22:28:07 -0400

    Final Project

---
+++ config/globals.js
@@ -0,0 +1,5 @@
+// array of global variables
+module.exports = {
+    // db: 'mongodb://localhost/comp2068-project' // local mongodb
+   db:'mongodb://s2018:webproject1@ds117422.mlab.com:17422/comp2068-project' // mlab
+};
